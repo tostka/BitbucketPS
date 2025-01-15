7ad8970929a168b8596efaf845cb4e50e19b13e7
Tostka <tostka@users.noreply.github.com>
2025-01-15 16:44:53 -0600

    pester v3tov5 update attempt chgs
    4:44 PM 1/15/2025

--- Tests/Set-BitBucketConfigServer.Tests.ps1
+++ Tests/Set-BitBucketConfigServer.Tests.ps1
@@ -1,10 +1,12 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-. "$here\$sut"
+﻿BeforeAll {
+    $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+    . "$here\$sut"
 
-InModuleScope PSBitBucket {
+    InModuleScope PSBitBucket {
 
-    $BitBucketServer = 'http://BitBucketserver.example.com'
+        $BitBucketServer = 'http://BitBucketserver.example.com'
+}
 
     Describe "Set-BitBucketConfigServer" {
 
@@ -12,7 +14,7 @@ InModuleScope PSBitBucket {
         Set-BitBucketConfigServer -Server $BitBucketServer -ConfigFile $configFile
 
         It "Ensures that a config.xml file exists" {
-            $configFile | Should Exist
+            $configFile | Should -Exist
         }
 
         $xml = New-Object -TypeName Xml
@@ -20,25 +22,25 @@ InModuleScope PSBitBucket {
         $xmlServer = $xml.Config.Server
 
         It "Ensures that the XML file has a Config.Server element" {
-            $xmlServer | Should Not BeNullOrEmpty
+            $xmlServer | Should -Not -BeNullOrEmpty
         }
 
         It "Sets the config file's Server value " {
-            $xmlServer | Should Be $BitBucketServer
+            $xmlServer | Should -Be $BitBucketServer
         }
 
         It "Trims whitespace from the provided Server parameter" {
             Set-BitBucketConfigServer -Server "$BitBucketServer " -ConfigFile $configFile
             $xml = New-Object -TypeName Xml
             $xml.Load($configFile)
-            $xml.Config.Server | Should Be $BitBucketServer
+            $xml.Config.Server | Should -Be $BitBucketServer
         }
 
         It "Trims trailing slasher from the provided Server parameter" {
             Set-BitBucketConfigServer -Server "$BitBucketServer/" -ConfigFile $configFile
             $xml = New-Object -TypeName Xml
             $xml.Load($configFile)
-            $xml.Config.Server | Should Be $BitBucketServer
+            $xml.Config.Server | Should -Be $BitBucketServer
         }
     }
 }
